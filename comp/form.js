21a81f8cde57f65a37b24f4a4ca04c08d754c81c
James Ngo <James.Ngo@toronto.ca>
2019-07-26 13:04:33 -0400

    Add URL Source for Form Definition

--- comp/form.js
+++ comp/form.js
@@ -5,30 +5,57 @@ const FormView = Backbone.BaseView.extend(
         this.removeChild(this.el.firstChild);
       }
 
-      const formDefinition = _.result(this, 'formDefinition');
-      formDefinition.id = _.result(formDefinition, 'id') || FormView.uniqueId();
-      formDefinition.rootPath = _.result(formDefinition, 'rootPath') || _.result(this, 'rootPath');
-      formDefinition.useBinding = true;
-
-      formDefinition.success = formDefinition.success || ((event) => {
-        event.preventDefault();
-        this.success();
-        return false;
-      });
-
-      this.cotForm = new CotForm(formDefinition);
-      this.cotForm.setModel(this.model);
-      this.cotForm.setView(this);
+      let formDefinition = _.result(this, 'formDefinition');
 
       return Promise.resolve()
         .then(() => {
-          return this.cotForm.render({ target: this.el });
+          if (typeof formDefinition === 'string') {
+            return ajax({
+              url: formDefinition
+            }).then((data) => {
+              formDefinition = data;
+            });
+          }
         })
         .then(() => {
-          this.form = this.el.querySelector('form');
-          this.formValidator = $(this.form).data('formValidation');
+          if (formDefinition.scripts) {
+            return loadScripts(...formDefinition.scripts);
+          }
+        })
+        .then(() => {
+          formDefinition.id = _.result(formDefinition, 'id') || FormView.uniqueId();
+          formDefinition.rootPath = _.result(formDefinition, 'rootPath') || _.result(this, 'rootPath');
+          formDefinition.useBinding = true;
 
-          return Backbone.BaseView.prototype.render.call(this);
+          // NOTE: Weird behaviour - Assigning functions into existing object changes the function's context to previous instance...
+          // So I had to keep function in temporary object by duplicating the definition.
+          const tempFormDefinition = {};
+          for (const key in formDefinition) {
+            if (formDefinition.hasOwnProperty(key)) {
+              tempFormDefinition[key] = formDefinition[key];
+            }
+          }
+
+          tempFormDefinition.success = formDefinition.success || ((event) => {
+            event.preventDefault();
+            this.success();
+            return false;
+          });
+
+          this.cotForm = new CotForm(tempFormDefinition);
+          this.cotForm.setModel(this.model);
+          this.cotForm.setView(this);
+
+          return Promise.resolve()
+            .then(() => {
+              return this.cotForm.render({ target: this.el });
+            })
+            .then(() => {
+              this.form = this.el.querySelector('form');
+              this.formValidator = $(this.form).data('formValidation');
+
+              return Backbone.BaseView.prototype.render.call(this);
+            });
         });
     },
 
